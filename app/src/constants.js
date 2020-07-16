e56ba003581c98a1bd842db43c45f67f291602c0
Rodrigo Perez <rodrigopi1989@gmail.com>
2020-07-16 18:03:51 -0300

    new constants

--- app/src/constants.js
+++ app/src/constants.js
@@ -2,7 +2,8 @@ export const BRIGHTID_CONTEXT = '1hive'
 
 export const NODE_URL = 'http:%2f%2fnode.brightid.org'
 
-export const BRIGHTID_VERIFICATION_ENDPOINT =
-  'http://node.brightid.org/brightid/v4/verifications'
+export const BRIGHT_ID_ENDPOINT = 'http://node.brightid.org/brightid/v4'
+export const BRIGHTID_VERIFICATION_ENDPOINT = `${BRIGHT_ID_ENDPOINT}/verifications`
+export const BRIGHTID_SUBSCRIPTION_ENDPOINT = `${BRIGHT_ID_ENDPOINT}/operations`
 
 export const BRIGHT_ID_APP_DEEPLINK = `brightid://link-verification/${NODE_URL}/${BRIGHTID_CONTEXT}`
