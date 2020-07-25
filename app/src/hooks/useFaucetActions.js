167c72543a89e4c38d269431320473e5c3a3751a
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2020-07-25 15:02:12 -0300

    Increase gas limir

--- app/src/hooks/useFaucetActions.js
+++ app/src/hooks/useFaucetActions.js
@@ -2,9 +2,13 @@ import { useCallback } from 'react'
 import { getNetwork } from '../networks'
 import { useContract } from './useWeb3Contracts'
 
+import { parseUnits } from '../lib/math-utils'
 import { formatBytes32String, sanitizeSignature } from '../lib/web3-utils'
 import { CONTEXT_ID } from '../constants'
 import brightIdFaucetAbi from '../abi/BrightIdFaucet.json'
+
+const GAS_LIMIT = 500000
+const GAS_PRICE = parseUnits('2', 'gwei')
 
 function useFaucetContract() {
   const faucetAddress = getNetwork().faucet
@@ -26,7 +30,7 @@ function useFaucetActions() {
         sig.v,
         sig.r,
         sig.s,
-        { gasLimit: 500000 }
+        { gasLimit: GAS_LIMIT, gasPrice: GAS_PRICE }
       )
     },
     [faucetContract]
