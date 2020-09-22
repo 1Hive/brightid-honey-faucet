d1e3a67cab8dbcf57f05c4600c7970fa5e63074a
Rodrigo Perez <rodrigopi1989@gmail.com>
2020-09-22 20:43:59 -0300

    new operations api version

--- app/src/endpoints.js
+++ app/src/endpoints.js
@@ -7,7 +7,7 @@ export const NODE_URL = 'http:%2f%2fnode.brightid.org'
 export const BRIGHT_ID_ENDPOINT_V4 = 'https://app.brightid.org/node/v4'
 export const BRIGHT_ID_ENDPOINT_V5 = 'https://app.brightid.org/node/v5'
 export const BRIGHTID_VERIFICATION_ENDPOINT = `${BRIGHT_ID_ENDPOINT_V5}/verifications`
-export const BRIGHTID_SUBSCRIPTION_ENDPOINT = `${BRIGHT_ID_ENDPOINT_V4}/operations`
+export const BRIGHTID_SUBSCRIPTION_ENDPOINT = `${BRIGHT_ID_ENDPOINT_V5}/operations`
 
 export const BRIGHT_ID_APP_DEEPLINK = `brightid://link-verification/${NODE_URL}/${CONTEXT_ID}`
 
--- app/src/services/sponsorUser.js
+++ app/src/services/sponsorUser.js
@@ -1,6 +1,5 @@
 import tweetNacl from 'tweetnacl'
-import tweetNaclUtils from 'tweetnacl-util'
-import sha256 from 'js-sha256'
+import stringify from 'fast-json-stable-stringify'
 
 import { CONTEXT_ID } from '../constants'
 import { BRIGHTID_SUBSCRIPTION_ENDPOINT } from '../endpoints'
@@ -15,23 +14,31 @@ export async function sponsorUser(account) {
       return { error: 'No private key found for the node' }
     }
 
-    const { key, signedMessage } = sponsorKeyAndSig(account, privateKey)
-    const endpoint = `${BRIGHTID_SUBSCRIPTION_ENDPOINT}/${key}`
+    const timestamp = Date.now()
+    const op = {
+      v: 5,
+      name: 'Sponsor',
+      app: CONTEXT_ID,
+      timestamp,
+      contextId: account,
+    }
+    const message = getMessage(op)
+    const messageUint8Array = Buffer.from(message)
+    op.sig = uInt8ArrayToB64(
+      Object.values(tweetNacl.sign.detached(messageUint8Array, privateKey))
+    )
+
+    const endpoint = `${BRIGHTID_SUBSCRIPTION_ENDPOINT}`
     const rawResponse = await fetch(endpoint, {
-      method: 'PUT',
+      method: 'POST',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({
-        name: 'Sponsor',
-        _key: key,
-        contextId: account,
-        context: CONTEXT_ID,
-        sig: signedMessage,
-        v: 4,
-      }),
+      body: op,
     })
+
+    console.log('raw response ', rawResponse)
 
     if (rawResponse.ok) {
       return {
@@ -56,25 +63,18 @@ export async function sponsorUser(account) {
   }
 }
 
-function sponsorKeyAndSig(account, privateKey) {
-  const privateKeyUint8Array = tweetNaclUtils.decodeBase64(privateKey)
+function uInt8ArrayToB64(array) {
+  const b = Buffer.from(array)
+  return b.toString('base64')
+}
 
-  const message = `Sponsor,${CONTEXT_ID},${account}`
-  const messageUint8Array = Buffer.from(message)
-  const messageSha256 = sha256.sha256.digest(message)
-  const messageBase64 = tweetNaclUtils.encodeBase64(messageSha256)
-  const messageRemoveNonUrlChars = messageBase64
-    .replace(/\+/g, '-')
-    .replace(/\//g, '_')
-    .replace(/=/g, '')
-
-  const signedMessageUint8Array = tweetNacl.sign.detached(
-    messageUint8Array,
-    privateKeyUint8Array
-  )
-  const encodedSignedMessage = tweetNaclUtils.encodeBase64(
-    signedMessageUint8Array
-  )
-
-  return { key: messageRemoveNonUrlChars, signedMessage: encodedSignedMessage }
+function getMessage(op) {
+  const signedOp = {}
+  for (const k in op) {
+    if (['sig', 'sig1', 'sig2', 'hash'].includes(k)) {
+      continue
+    }
+    signedOp[k] = op[k]
+  }
+  return stringify(signedOp)
 }
